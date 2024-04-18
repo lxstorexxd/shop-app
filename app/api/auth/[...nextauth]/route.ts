8485b33ddc3a2f71a7188d001dca6bb95ccb1dbe
Orexxd <75565271+lxstorexxd@users.noreply.github.com>
2024-04-18 09:28:43 +0300

    Create route.ts

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,49 @@
+import NextAuth, { AuthOptions } from "next-auth";
+import VkProvider from "next-auth/providers/vk";
+import YandexProvider from "next-auth/providers/yandex";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "@/lib/prisma";
+
+const handler = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    VkProvider({
+      clientId: process.env.VK_CLIENT_ID as string,
+      clientSecret: process.env.VK_CLIENT_SECRET as string,
+    }),
+    YandexProvider({
+      clientId: process.env.YANDEX_CLIENT_ID as string,
+      clientSecret: process.env.YANDEX_CLIENT_SECRET as string,
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+    maxAge: 60 * 60 * 24,
+  },
+  secret: process.env.NEXTAUTH_SECRET as string,
+  callbacks: {
+    async jwt({ token, user, session }) {
+      console.log("jwt callback", { token, user, session });
+      if (user) {
+        return {
+          ...token,
+          id: user.id,
+        };
+      }
+      return token;
+    },
+
+    async session({ session, token, user }) {
+      console.log("session callback", { session, token, user });
+      return {
+        ...session,
+        user: {
+          ...session.user,
+          id: token.id,
+        },
+      };
+    },
+  },
+});
+
+export { handler as GET, handler as POST };
