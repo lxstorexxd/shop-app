74a914afeb38de20f40122ca1d18cb3000f7d317
Orexxd <75565271+lxstorexxd@users.noreply.github.com>
2024-04-26 10:34:09 +0300

    Add files via upload

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,11 +1,15 @@
 "use client";
 import { NextUIProvider } from "@nextui-org/react";
 import { ContextProvider } from "@/components/CartContext";
+import { Toaster } from "sonner";
 
 export async function Providers({ children }: { children: React.ReactNode }) {
   return (
     <NextUIProvider>
-      <ContextProvider>{children}</ContextProvider>
+      <ContextProvider>
+        {children}
+        <Toaster />
+      </ContextProvider>
     </NextUIProvider>
   );
 }
