4b32fc6f8a0e15b899e095eb58c196caf6e5a4ab
Orexxd <75565271+lxstorexxd@users.noreply.github.com>
2024-05-07 17:52:58 +0300

    Delete action directory

--- action/get-categories/index.ts
+++
@@ -1,9 +0,0 @@
-"use server";
-import prisma from "@/lib/prisma";
-
-const getCategories = async () => {
-  const categories = (await prisma.category.findMany()) ?? [];
-  return categories;
-};
-
-export { getCategories };
